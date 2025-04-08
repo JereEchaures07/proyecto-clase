4286f65310c50aa37f35919ef5a9752eafb0af96
JereEchaures07 <jerechaures@gmail>
2025-04-08 11:02:01 -0300

    creacion de el proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { InicioComponent } from './inicio/inicio/inicio.component';
 import { FormularioComponent } from './contacto/formulario/formulario.component';
+import { ProductoComponent } from './productos/producto/producto.component';
 
 export const routes: Routes = [
     {path: '',redirectTo:'/inicio',pathMatch:'full'},
     {path:'inicio', component:InicioComponent},
-    {path:'contacto',component:FormularioComponent}
-
-
+    {path:'contacto',component:FormularioComponent},
+    {path:'productos',component:ProductoComponent}
 ];
