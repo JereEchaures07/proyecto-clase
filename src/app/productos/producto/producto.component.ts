ad0515bef6cac696c628d6dbe581ddca6b56f783
JereEchaures07 <jerechaures@gmail>
2025-04-10 11:01:45 -0300

    proyectoclase

--- src/app/productos/producto/producto.component.ts
+++ src/app/productos/producto/producto.component.ts
@@ -1,11 +1,29 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-producto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.css'
 })
 export class ProductoComponent {
+  /* productos = [
+     { nombre: "productos 1", precio: 100 },
+     { nombre: "productos 2", precio: 150 },
+     { nombre: "productos 3", precio: 200 },
+   ]
+ }
+
+  usuario = {
+    nombre: 'jere',
+    activo:false,
+  };*/
+
+  usuario = {
+    nombre: ''
+  };
+
 
 }
